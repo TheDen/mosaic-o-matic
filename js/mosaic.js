58cef6ada9396fcec16c58f14c9821dca2cd65a0
denis <deniskhoshaba@gmail.com>
2016-06-27 11:06:39 +1000

    uploads

---
+++ js/mosaic.js
@@ -0,0 +1,11 @@
+// Constants shared between client and server.
+
+var TILE_WIDTH = 20;
+var TILE_HEIGHT = 20;
+
+var exports = exports || null;
+if (exports) {
+  exports.TILE_WIDTH = TILE_WIDTH;
+  exports.TILE_HEIGHT = TILE_HEIGHT;
+}
+
